d1fa109f151cc041cf3ce9c4fb44393dcb7245ce
Anna Fuentes Jakobsen <annafjakobsen@gmail.com>
2017-07-06 10:23:48 +0200

    added some styleing to search-bar

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+require('./style/style.css');
 
 const API_KEY = 'AIzaSyDji4tv5VLrrrVE8Oedv-0mqCm9ywlW07Q';
 
